9583deec9a0c2f1636a16c7b7b84888966bc754e
antonella <antonella_caroca@hotmail.com>
2018-05-28 02:58:33 -0400

    archivo js del primer ejercicio

---
+++ app.js
@@ -0,0 +1,16 @@
+// 1. Funciones matemáticas
+
+function group (c,d){
+    function add (a,b){
+        return a+b;
+    }
+    function multiply(x,y){
+        return x*y;
+    }
+    return (36325*(9824+777)); 
+    
+}
+
+
+var div = document.getElementById("ejercicio1");
+div.textContent = "El resultado del ejercicio 1 es  " + group();
